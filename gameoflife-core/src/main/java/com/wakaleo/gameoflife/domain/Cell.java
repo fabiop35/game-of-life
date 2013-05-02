034b5016a11f33203566dc634b2f9b968d595566
root <root@fc07.(none)>
2013-05-01 22:26:20 -0500

    Changes stars to pluses 3

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -1,13 +1,15 @@
 package com.wakaleo.gameoflife.domain;
 
 public enum Cell {
-    
+  
     LIVE_CELL("+"), DEAD_CELL(".");
 
     private String symbol;
 
     private Cell(final String initialSymbol) {
+
         this.symbol = initialSymbol;
+
     }
 
     @Override
